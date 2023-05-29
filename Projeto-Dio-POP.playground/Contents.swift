5200b3d3e25521f78e98c81dd3b851a260ae1f9e
André Toti <detoti@gmail.com>
2023-05-29 09:40:51 -0300

    POP file - Updated - First code

--- Projeto-Dio-POP.playground/Contents.swift
+++ Projeto-Dio-POP.playground/Contents.swift
@@ -1,3 +1,50 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol Printable {
+    mutating func printDocument()
+    mutating func printImage()
+}
+
+protocol TonerReloadable {
+    mutating func reloadToner()
+}
+
+protocol TonerLevelCheckable {
+    var tonerLevel: Int { get }
+}
+
+struct Printer: Printable, TonerReloadable, TonerLevelCheckable, CustomStringConvertible {
+    mutating func printDocument() {
+        print("O documento foi impresso.")
+        tonerLevel -= 20
+    }
+    
+    mutating func printImage() {
+        print("A imagem foi impressa.")
+        tonerLevel -= 30
+    }
+    
+    mutating func reloadToner() {
+        tonerLevel = 100
+        print("O toner foi recarregado. Nivel atual: \(tonerLevel)%.")
+    }
+    
+    private(set) var tonerLevel: Int
+    
+    init (tonerLevel: Int = 100) {
+        self.tonerLevel = max(min(tonerLevel, 100), 0)
+    }
+
+    
+    var description: String {
+        return "Nivel do toner: \(tonerLevel)%."
+    }
+}
+
+var myPrinter = Printer()
+print(myPrinter)
+myPrinter.printDocument()
+myPrinter.printImage()
+print(myPrinter)
+myPrinter.reloadToner()
+print(myPrinter)
